4d7377657a777acdfd80fca3bb9f83f0cacb1caa
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2021-09-13 16:04:21 -0600

    Pausing for workqueue impl

---
+++ network_private.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file network_private.hpp
+ * @author Sunip K. Mukherjee (sunipkmukherjee@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef NETWORK_PRIVATE_HPP
+#define NETWORK_PRIVATE_HPP
+
+#define SRV_AUTH_TOKEN 0x41
+
+#endif
